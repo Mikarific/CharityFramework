b8e7f8e3082f38fbc2fc50bd398f48a549f88d26
Mikarific <mikerific2@gmail.com>
2025-08-15 09:23:03 -0600

    Plugins UI

--- src/ui/tabs/plugins.tsx
+++ src/ui/tabs/plugins.tsx
@@ -1,3 +1,5 @@
+import { For } from 'solid-js';
+
 import { addPlugin } from '../../plugins/loader';
 import styles from '../styles/panel.module.css';
 
@@ -18,13 +20,27 @@ export function Plugins() {
 						try {
 							await addPlugin(input.value);
 						} catch (err) {
-							console.error('[Charity] Failed to add plugin!', err);
+							console.error('[Charity]', err);
 						}
 					}}
 					onMouseDown={(e) => e.stopPropagation()}
 				>
 					Add
 				</button>
+			</div>
+			<div>
+				<For each={window.charity.internal.plugins}>
+					{(plugin) => {
+						return (
+							<div class={styles.plugin}>
+								<div class={styles.pluginName}>
+									{plugin.manifest.name}&nbsp;<span class={styles.gray}>by {plugin.manifest.authors.join(', ')}</span>
+								</div>
+								<button onMouseDown={(e) => e.stopPropagation()}>Remove</button>
+							</div>
+						);
+					}}
+				</For>
 			</div>
 		</div>
 	);
